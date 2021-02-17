74b2b8aeab16e575016ef7feeb5a9aec6e5a9929
Mariusz Bińkowski <binkowski5808@portal.ron.mil.pl>
2021-02-17 18:53:03 +0100

    Init commit for microfrontend, currently only monorepo

--- README.md
+++
@@ -1,3 +0,0 @@
-# microfrontend-React-App
-
-Applicaiton which gathers multiple React application and sort it by routes to make it more module-like
---
+++ microfrontend-react-app/mainapp/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import { AuthProvider } from "@microfrontend-react-app/shared-library";
+
+function App() {
+  return (
+    <div>
+      <h1>Context Provider</h1>
+      <h2>App 1</h2>
+      <AuthProvider.Provider value={{ isLogged: true }}></AuthProvider.Provider>
+    </div>
+  );
+}
+
+export default App;
